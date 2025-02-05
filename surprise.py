3095d9e4d1345f3a36d810ba770ccbbed187e5bc
quinnhasse <quinn.hasse@gmail.com>
2025-02-05 17:07:22 -0600

    Auto-generated code for: Make a surprise in python with whatever you'd like, for my girlfriend. I want to surprise her with something super cool that she will love! Please make it in a file called 'surprise.py', make sure it is able to run out of the box, but feel free to make whatever you think she would find super cool! Thanks!

--- surprise.py
+++ surprise.py
@@ -1,26 +1,18 @@
-import turtle
+# surprise.py
 
-# Create a screen
-screen = turtle.Screen()
-screen.bgcolor("black")
-screen.title("Surprise Art")
+def create_digital_love_letter():
+    love_letter = """
+    Dear [Girlfriend's Name],
 
-# Create a turtle
-artist = turtle.Turtle()
-artist.speed(0)
-artist.width(2)
+    I just wanted to take a moment to express my love for you. You mean the world to me and I am so grateful to have you in my life. Your smile brightens my day and your laughter is music to my ears.
 
-# Set color palette
-colors = ["red", "yellow", "blue", "green", "orange", "purple"]
+    Every moment spent with you is like a beautiful dream and I cherish the time we share together. You are my rock, my inspiration, and my everything. I love you more than words can express.
 
-# Draw a beautiful spiral design
-for i in range(360):
-    artist.color(colors[i % 6])
-    artist.forward(i * 2)
-    artist.right(59)
+    Forever yours,
+    [Your Name]
+    """
 
-# Hide the turtle and display the artwork
-artist.hideturtle()
+    print(love_letter)
 
-# Exit on click
-screen.exitonclick()
+if __name__ == "__main__":
+    create_digital_love_letter()
